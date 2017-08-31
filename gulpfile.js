7a73ee2b22657121daa3d5a69f73da66f6ca470d
Javier Moreno <javier.moreno@basiko.mx>
2017-08-31 12:52:44 -0500

    Inicialización de archivos para producción de desarrollo.

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp')
+var postcss = require('gulp-postcss')
+var browserSync = require('browser-sync').create()
+
+//Servidor de desarrollo
+gulp.task('serve', function () {
+	browserSync.init({
+		server:{
+			baseDir: './dist'
+		}
+	})
+})
+
+//Tarea para procesar el CSS
+gulp.task('css', function() {
+
+	var processor = []
+
+	return gulp.src('./src/*.css')
+	.pipe(postcss(processor))
+	.pipe(gulp.dest('./dist/css'))
+	.pipe(browserSync.stream())
+})
+
+// Tarea para vigilar los cambios
+gulp.task('watch', function(){
+	gulp.watch('./src/*.css', ['css'])
+	gulp.watch('./dist/*.html').on('change', browserSync.reload)
+})
+
+gulp.task('default', ['watch', 'serve'])
